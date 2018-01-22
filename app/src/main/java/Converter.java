47b5be7794f88364dae54defffdaebf6593d162b
Sashen <sashenpillay943@gmail.com>
2018-01-22 10:50:34 +0200

    Unit test added
    Small average function created so that a unit test could be run

---
+++ app/src/main/java/Converter.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by SPillay on 2018/01/22.
+ */
+
+public class Converter {
+}
---
+++ app/src/test/java/ConverterTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by SPillay on 2018/01/22.
+ */
+public class ConverterTest {
+
+}
